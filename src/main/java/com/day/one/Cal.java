3c6c6c5ddd0b5a7cc1bac38a1aa45da7b9f185a2
Yousuf Yosuuf <yousufsiddiq78@gmail.com>
2022-07-27 21:24:13 -0700

    recently bright some changes in tekschool karate project

---
+++ src/main/java/com/day/one/Cal.java
@@ -0,0 +1,13 @@
+package com.day.one;
+
+public class Cal {
+	public static void main(String[] args) {
+		
+		int num1 = 5;
+		int num2 = 7;
+		int total = num1 + num2;
+		System.out.println(total);
+		System.out.println("read this book first");
+	}
+
+}
